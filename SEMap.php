f8bf5d359235b94d4927c0e22d7ab39148371d7f
root <root@localhost>
2017-11-15 19:37:29 +0100

    first checkin

---
+++ SEMap.php
@@ -0,0 +1,455 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>SE Map</title>
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1">
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js"  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="  crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/cytoscape/3.2.5/cytoscape.min.js"></script>
+		<script src="https://cdn.rawgit.com/cytoscape/cytoscape.js-cola/1.6.0/cola.js"></script>
+		<script src="https://cdn.rawgit.com/cytoscape/cytoscape.js-cola/1.6.0/cytoscape-cola.js"></script>
+        <link rel="stylesheet" href="https://gitcdn.link/repo/Chalarangelo/mini.css/master/dist/mini-default.min.css">
+		<style>
+            body { margin: 0; }
+            .responsive-label { align-items: center; }
+            .wide  {  margin: auto; }
+            #cy 
+            {
+                border: solid;
+                border-width: 1;
+                height: calc(100vh - 70px);
+            }
+		</style>
+		<script>
+		    var credentials = { username: "", password : ""};
+		
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		    // Database loading, deleting, saving
+		    //
+            function POST(eles, successFunction)
+            {
+                var nodes = [];
+                var edges = [];                    
+                for(var i=0;i<eles.length;i++)
+                {
+                    var e = eles[i];
+                    if (e.isNode())
+                    {
+                        nodes.push({id:e.id(),l:e.data("label"), x:e.position().x, y:e.position().y});
+                    }
+                    else if (e.isEdge())
+                    {
+                        edges.push({id:e.id(), s:e.data("source"), t:e.data("target"), l:e.data("label") });
+                    }
+                }
+                
+                $.ajax({
+                  type: "POST",
+                  url: "SEMapDatabase.php",
+                  dataType: "text",
+                  data: JSON.stringify({nodes:nodes, edges:edges, credentials:credentials }),
+                  success: successFunction
+                });
+            }
+            
+            function DELETE(eles, successFunction)
+            {
+                var nodes = [];
+                var edges = [];                    
+                for(var i=0;i<eles.length;i++)
+                {
+                    var e = eles[i];
+                    if (e.isNode())
+                    {
+                        nodes.push(e.id());
+                    }
+                    else if (e.isEdge())
+                    {
+                        edges.push(e.id());
+                    }
+                }
+                
+                $.ajax({
+                  type: "DELETE",
+                  url: "SEMapDatabase.php",
+                  dataType: "json",
+                  contentType: "application/json; charset=utf-8",
+                  data: JSON.stringify({nodes:nodes, edges:edges, credentials:credentials }),
+                  success : successFunction
+                });
+            }
+
+            function LoadDataFromDatabase()
+            {
+                // load elements from database and build diagram                
+                $.get("SEMapDatabase.php", function(data)
+                {
+                    var data = JSON.parse(data);
+                    
+                    var elms = []
+                    
+                    var nodes = data["nodes"];
+                    for(var node in nodes)
+                    {
+                        var n = nodes[node];
+                        elms.push( { group: "nodes", data: { id: n["id"], label: n["q"] }, position:{x:n["x"], y:n["y"]} })                      
+                    }
+
+                    var edges = data["edges"];
+                    for(var edge in edges)
+                    {
+                        var e = edges[edge];
+                        elms.push( { group: "edges", data: { id: e["id"], source: e["s"], target: e["t"], label: e["l"] } })                      
+                    }
+                    cy.add(elms);
+                    SetStyle();
+                    cy.fit();
+                    cy.autolock( true );
+                });
+            }
+
+            function SetStyle()
+            {
+                var nds = cy.nodes()
+                for(var i=0;i<nds.length;i++)
+                {
+                    if (nds[i].incomers().length==0)
+                    {
+                        nds[i].style({'border-color': 'red', "border-width":10})
+                    }
+                    else
+                    {
+                        nds[i].style({'border-color': 'gray', "border-width":0})
+                    }
+                }                
+            }
+
+			document.addEventListener('DOMContentLoaded', function()
+            {
+                ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                // Login stuff
+                //
+                function loggedIn()
+                {
+                    return $("#loginlogout").text()=="Logout";
+                }
+                
+                function logOut()
+                {
+                    $("#loginlogout").text("LogIn");        
+                    credentials["username"] = "";
+                    credentials["password"] = "";
+                    $(".admin").hide();
+                }
+    
+                $( "#loginlogout" ).click(function() 
+                {
+                    if (loggedIn())
+                    {
+                        logOut();
+                        cy.autolock( true );
+                    }
+                    else
+                    {
+                        $("#modal-login").prop("checked", true);
+                        cy.autolock( false );
+                    }
+                });
+                
+                $( "#login" ).click(function() 
+                {
+                    credentials["username"] = $("#username").val();
+                    credentials["password"] = $("#password").val();
+                    $.ajax({
+                        type: "POST",
+                        url: "SEMapDatabase.php",
+                        dataType: "json",
+                        data: JSON.stringify({credentials}),
+                        success: function(res)
+                        {
+                            if (res.res=="OK")
+                            {
+                                $("#modal-login").prop("checked", false);
+                                $("#loginlogout").text("Logout");        
+                                $(".admin").show();
+                            }
+                            else
+                            {
+                                alert(res.res);
+                            }
+                        }
+                    });
+                });
+
+                $(".admin").hide();
+
+
+                ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                // Handle graph's UI
+                //
+				var cy = window.cy = cytoscape(
+				{
+					container: document.getElementById('cy'),
+                    boxSelectionEnabled: false,
+                    
+                    minZoom: 0.1,
+                    maxZoom: 100,
+                    wheelSensitivity: 0.1,
+                    
+					style: 
+					[
+						{
+							selector: 'node',
+							css: 
+							{
+								'content': 'data(label)',
+                                'text-valign': 'center',
+                                'text-halign': 'center',
+                                'shape': 'roundrectangle',
+                                'text-wrap': 'wrap',   
+                                'width': '300px',       
+                                'text-max-width' : '300px',
+                                'height': '100px',                      
+							}
+						},
+						{
+							selector: 'edge',
+							css: 
+                            {
+                                'curve-style': 'bezier',
+                                'label': 'data(label)',
+								'target-arrow-shape': 'triangle'
+							}
+						}
+					]
+				});
+
+                // tapholding brings up a dialog to edit the node and the enges text
+                cy.on('taphold', 'node', function(evt)
+                {
+                    if (!loggedIn()) return;
+                    
+                    $("#dialog_label").text("Question")
+                    $("#dialog_input").attr("data", evt.target.id());
+                    $("#dialog_input").val(evt.target.data().label)
+                    $("#modal-edit").prop("checked", true);
+                });
+
+                // tapholding brings up a dialog to edit the node and the enges text
+                cy.on('taphold', 'edge', function(evt)
+                {
+                    if (!loggedIn()) return;
+                    
+                    $("#dialog_label").text("Answer")
+                    $("#dialog_input").attr("data", evt.target.id());
+                    $("#dialog_input").val(evt.target.data().label)
+                    $("#modal-edit").prop("checked", true);
+                });
+
+                // save handler for the text editing dialog
+                $( "#save" ).click(function()
+                {
+                    if (!loggedIn()) return;
+                    
+                    var q = $("#dialog_input")[0];
+                    var id = q.attributes["data"].nodeValue;
+                    cy.$( "#"+id ).data("label", q.value);
+                    POST(cy.$( "#"+id ));
+                    $("#modal-edit").prop("checked", false);
+                });                   
+
+                // 
+                function generateRandomID()
+                {
+                    var id = ""+ Math.random();                    
+                    return id.split(".").join("");
+                }
+
+                //right clicking on a edge inserts a new node
+                cy.on('cxttap', 'edge', function(evt)
+                {
+                    if (!loggedIn()) return;
+                    
+                    var source = evt.target.source();
+                    var target = evt.target.target();
+                    
+                    var sp = source.position();
+                    var st = target.position();
+                    
+                    var id = generateRandomID()
+                    
+                    var eles = cy.add([
+                      { group: "nodes", data: { id: id, label: "New Node" }, position: { x: (sp.x + st.x)*.5, y: (sp.y + st.y)*.5 } },                      
+                      { group: "edges", data: { id: generateRandomID(), source: source.id(), target: id } },
+                      { group: "edges", data: { id: generateRandomID(), source: id, target: target.id(), label: evt.target.data().label } }
+                    ]);
+                    POST(eles, function(data)
+                    {
+                        var eles = cy.remove(cy.getElementById(evt.target.id()))
+                        SetStyle();
+                        DELETE( eles );
+                    });
+                });
+
+                //right clicking on the background creates a node
+                cy.on('cxttap', function(evt)
+                {   
+                    if (!loggedIn()) return;
+                    
+                    if (evt.target===cy)
+                    {                    
+                        var eles = cy.add([{ group: "nodes", data: { id: generateRandomID(), label: "No question" }, renderedPosition: evt.originalEvent }]);
+                        POST(eles);
+                    }
+                });
+                
+                // shift selecting 2 nodes creates a link
+                cy.on('select', 'node', function(evt)
+                {
+                    if (!loggedIn()) return;
+                    
+                    var node = evt.target;
+                    
+                    var selected = cy.elements(":selected");
+                    if (selected.length==2)
+                    {
+                        var source = (node.id()==selected[1].id())?selected[0].id():selected[1].id();
+                        var target = node.id();
+                    
+                        var eles = cy.add([{ group: "edges", data: { id: generateRandomID(), source: source, target: target, label: "No answer"  } }]); 
+                        SetStyle();
+                        POST(eles);
+                    }                    
+                });
+
+                // the delete key erases the selected nodes (sometimes it doesnt work! :( )
+                $('html').keydown(function(e)
+                {
+                    if(e.keyCode == 46) 
+                    {
+                        if($(event.target).is('textarea') || $(event.target).is('input')) 
+                            return;
+
+                        if (!loggedIn()) return;
+                        
+                        var eles = cy.remove(":selected");
+                        DELETE(eles);
+                    }
+                });
+                
+                $( "#applyLayout" ).click(function() 
+                {
+                    if (!loggedIn()) return;
+                    
+                    cy.layout({
+						name: 'cola',
+                        edgeLength:400
+					}).start(10)
+                });
+
+                $( "#saveDatabase" ).click(function() 
+                {
+                    if (!loggedIn()) return;
+                    
+                    POST(cy.nodes());
+                });
+
+                // Load graph from database
+                //
+                LoadDataFromDatabase();
+                $("#modal-help").prop("checked", true);
+			});
+        
+		</script>
+	</head>
+	<body>
+	    
+	    <!-- header -->
+        <header class="sticky">
+          <a href="#" class="logo">SE Map</a>
+          <button onclick="$('#modal-help').prop('checked', true);" >Help</button>
+          <button class="admin" id="applyLayout" >Apply Layout</button>
+          <button class="admin" id="saveDatabase">Save</button>
+          <button id="loginlogout" style="float: right">Login</button>
+        </header>
+        
+        <!-- graph area-->
+        <div class="container">
+            <div class="content">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12">
+                        <div id="cy"></div>
+                    </div>            
+                </div>            
+            </div>         
+        </div>
+
+        <!-- editing dialog -->
+        <input id="modal-edit" type="checkbox"/>
+        <div class="modal">
+          <div class="wide" style="width:750px">
+            <label for="modal-edit"></label>
+            <form>
+                <h3 class="section">Edit field</h3>
+                <fieldset>
+                    <div class="row responsive-label">
+                        <div class="col-sm-12 col-md-3">
+                            <label for="question" id="dialog_label">Question</label>
+                        </div>
+                        <div class="col-sm-12 col-md">
+                            <textarea type="text" id="dialog_input" style="width:85%;" rows="3"></textarea>
+                        </div>
+                    </div>
+                    <div class="input-group fluid">
+                      <button type="button" class="primary" id="save">Save</button>
+                      <button type="button" class="cancel" onclick='$("#modal-edit").prop("checked", false);'>Cancel</button>
+                    </div>
+                </fieldset>
+            </form>
+          </div>
+        </div>
+
+        <!-- login dialog -->
+        <input id="modal-login" type="checkbox"/>
+        <div class="modal">
+            <div class="wide">
+            <!-- Adding some flex properties to center the form and some height to the page, these can be omitted -->
+              <label for="modal-login"></label>
+              <form >
+                <fieldset>
+                  <legend>Login form</legend>
+                    <div class="input-group fluid">
+                      <label for="username" style="width: 80px;">Username</label>
+                      <input name="username" value="" id="username" placeholder="username" type="text" >
+                    </div>
+                    <div class="input-group fluid">
+                      <label for="pwd" style="width: 80px;">Password</label>
+                      <input name="password" value="" id="password" placeholder="password" type="password">
+                    </div>
+                    <div class="input-group fluid">
+                      <button type="button" class="primary" id="login">Login</button>
+                      <button type="button" class="cancel" onclick='$("#modal-login").prop("checked", false);'>Cancel</button>
+                    </div>
+                </fieldset>
+              </form>
+            </div>
+        </div>
+
+        <!-- help dialog -->
+        <input id="modal-help" type="checkbox"/>
+        <div class="modal">
+            <div class="wide">
+                <div class="card large">
+                    <label for="modal-help" class="close"></label>
+                    <div class="section">
+                        <h3>Quick start</h3>
+                        <ul>
+                            <li>Start in the highlighted box and go from there.</li>
+                            <li>Controls: same as google maps.</li>
+                        </ul>
+                    </div>
+                </div>                
+            </div>
+        </div>
+        
+	</body>
+</html>
---
+++ SEMapDatabase.php
@@ -0,0 +1,216 @@
+<?php
+    function escapeJsonString($value) 
+    { 
+        # list from www.json.org: (\b backspace, \f formfeed)
+        $escapers = array("\\", "/", "\"", "\n", "\r", "\t", "\x08", "\x0c");
+        $replacements = array("\\\\", "\\/", "\\\"", "\\n", "\\r", "\\t", "\\f", "\\b");
+        $result = str_replace($escapers, $replacements, $value);
+        return $result;
+    }
+
+    function ValidateCredentials($data) 
+    {
+        $users = array("user" => "userpass");
+        
+        if (array_key_exists("credentials", $data)==false)
+        {   
+            die('{"res":"need credentials"}');
+            return;
+        }
+        
+        $credentials = $data["credentials"];
+        
+        if (array_key_exists("username", $credentials)==true && array_key_exists("password", $credentials)==true)
+        {
+            $username = $credentials["username"];
+            $password = $credentials["password"];
+            
+            if (array_key_exists($username, $users))
+            {
+                if ($users[$username]!=$password)
+                {   
+                    die('{"res":"bad login/passwd"}');
+                }
+            }
+            else
+            {
+                die('{"res":"bad login/passwd"}');
+            }
+        }
+        else
+        {
+            die('{"res":"login/pass not set"}');
+        }
+    }
+
+    $ACCESS_PWD='';
+    
+    date_default_timezone_set("Europe/Brussels"); 
+    
+    header("Content-Type: text/plain; charset=us-ascii");
+    header_remove("Transfer-Encoding"); 
+    header_remove("Connection");
+
+    // Create connection
+    $conn = new SQLite3("/storage/sdcard0/www/SEMap/SEMap.sqlite");
+    if ($conn===false) 
+    {
+        echo "Failed to connect to MySQL:  " . $conn->lastErrorMsg();
+    }
+
+    if (!($conn->exec("CREATE TABLE IF NOT EXISTS NODES (id TEXT UNIQUE, label TEXT, x FLOAT, y FLOAT)")))
+    {
+        die( "Error creating NODES table" . $conn->lastErrorMsg());
+    }
+
+    if (!($conn->exec("CREATE TABLE IF NOT EXISTS EDGES (id TEXT UNIQUE, source TEXT, target TEXT, answer TEXT)")))
+    {
+        die( "Error creating EDGES table" . $conn->lastErrorMsg());
+    }
+
+    $method =  $_SERVER['REQUEST_METHOD'];
+
+    if ($method=="POST") 
+    {
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        validateCredentials($data);
+
+        if (!($conn->exec("BEGIN TRANSACTION")))
+        {
+            die( $conn->lastErrorMsg());
+        } 
+
+        if (array_key_exists("nodes", $data)==true)
+        {
+            
+            $nodelist = $data["nodes"];
+            foreach ($nodelist as $node) 
+            {
+                $id = SQLite3::escapeString($node["id"]);
+                $l  = SQLite3::escapeString($node["l"]);
+                $x  = SQLite3::escapeString($node["x"]);
+                $y  = SQLite3::escapeString($node["y"]);
+                
+                $str = "insert or replace INTO NODES values( '$id', '$l', '$x', '$y' )";
+                if (!$conn->exec($str)) 
+                {
+                    die( "Insert failed: $str " . $conn->lastErrorMsg());
+                }
+            }
+        }
+    
+        if (array_key_exists("edges", $data)==true)
+        {
+            $edgelist = $data["edges"];
+            foreach ($edgelist as $edge) 
+            {
+                $id = SQLite3::escapeString($edge["id"]);
+                $s  = SQLite3::escapeString($edge["s"]);
+                $t  = SQLite3::escapeString($edge["t"]);
+                $l  = SQLite3::escapeString($edge["l"]);
+                
+                $str = "insert or replace INTO EDGES values( '$id', '$s', '$t', '$l' )";
+                if (!$conn->exec($str)) 
+                {
+                    die( "Insert failed: $str" . $conn->lastErrorMsg());
+                }
+            }
+        }
+        
+        if (!($conn->exec("END TRANSACTION")))
+        {
+            die( $conn->lastErrorMsg());
+        } 
+        
+        echo '{"res":"OK"}';
+    } 
+    else if ($method=="GET") 
+    {
+        echo '{"nodes":[';
+
+        $results = $conn->query('SELECT * FROM "NODES"');
+        $counter = 0;        
+        while ($row = $results->fetchArray(1)) 
+        {
+            if ($counter!=0)
+                echo ',';
+            $id = $row["id"];
+            $q =  escapeJsonString($row["label"]);
+            $x =  $row["x"];
+            $y =  $row["y"];
+            echo '{"id":"'.$id.'", "q":"'.$q.'", "x":'.$x.', "y":'.$y.'}';
+            $counter++;
+        }
+        
+        echo "],";
+
+        echo '"edges":[';
+
+        $results = $conn->query('SELECT * FROM "EDGES"');
+        $counter = 0;        
+        while ($row = $results->fetchArray(1)) 
+        {
+            if ($counter!=0)
+                echo ',';
+
+            $id = $row["id"];
+            $s = $row["source"];
+            $t = $row["target"];
+            $l =  escapeJsonString($row["answer"]);
+            echo '{"id":"'.$id.'","s":"'.$s.'","t":"'.$t.'","l":"'.$l.'"}';
+
+            $counter++;
+        }
+        
+        echo "] }";
+        
+    }
+    else if ($method=="DELETE") 
+    {
+        $data = json_decode(file_get_contents('php://input'), true);
+        
+        validateCredentials($data);
+        
+        if (!($conn->exec("BEGIN TRANSACTION")))
+        {
+            die( $conn->lastErrorMsg());
+        } 
+
+        if (array_key_exists("nodes", $data)==true)
+        {
+            $idlist = $data["nodes"];
+            foreach ($idlist as $id) 
+            {
+                $str = "delete from NODES where id='$id'";
+                if (!$conn->exec($str)) 
+                {
+                    die( "Insert failed: $str " . $conn->lastErrorMsg());
+                }
+            }
+        }
+    
+        if (array_key_exists("edges", $data)==true)
+        {
+            $idlist = $data["edges"];
+            foreach ($idlist as $id) 
+            {
+                $str = "delete from EDGES where id='$id'";
+                if (!$conn->exec($str)) 
+                {
+                    die( "Insert failed: $str " . $conn->lastErrorMsg());
+                }
+            }
+        }
+        
+        if (!($conn->exec("END TRANSACTION")))
+        {
+            die( $conn->lastErrorMsg());
+        } 
+        
+        echo '{"res":"OK"}';
+    }    
+    
+    
+    $conn->close();
+?>
